c511e129d2b9b3ec7f2007ba5aa35ed4052453ab
kmccloy <35931432+kmccloy@users.noreply.github.com>
2026-07-24 10:29:58 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import PortfolioBot from "./PortfolioBot.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PortfolioBot />
+    <Analytics />
   </React.StrictMode>
 );
