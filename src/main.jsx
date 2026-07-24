1d23650db12c7e3560627edf302488b5a2fd9d40
kmccloy <35931432+kmccloy@users.noreply.github.com>
2026-07-24 10:33:24 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics";
 import PortfolioBot from "./PortfolioBot.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
